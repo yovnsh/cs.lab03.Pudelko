fe2345176156402234cee01d6af5ad0d9692f8ca
gabriela goliszke <yovnsh@gmail.com>
2023-03-25 15:24:25 +0100

    śmierdzi kwadrat

---
+++ Pudełko/Pudełko/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace KwadratŚmierdzący
+{
+    using P = KwadratŚmierdzący.Pudelko;
+    sealed class Pudelko
+    {
+        double a { get; set; }
+        double b { get; set; }
+        double c { get; set; }
+
+        public Pudelko (double a = 10, double b = 10, double c = 10)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+}
