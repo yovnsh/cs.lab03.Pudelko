2c644107d3542eeec49b929938421c20e748c77a
gabriela goliszke <yovnsh@gmail.com>
2023-04-23 18:04:26 +0200

    wypudlaj

---
+++ Pudełko/dowolna/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace pudla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("help");
+        }
+    }
+
+}
---
+++ Pudełko/dowolna/pudlowpudle.cs
@@ -0,0 +1,18 @@
+﻿using KwadratŚmierdzący;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dowolna
+{
+    public static class pudlowpudle
+    {
+        public static Pudelko Kompresuj(this Pudelko wypudlaj)
+        {
+            double boka = Math.Pow(wypudlaj.Objetosc, (1.0 / 3.0));
+            return new Pudelko(boka, boka, boka);
+        }
+    }
+}
